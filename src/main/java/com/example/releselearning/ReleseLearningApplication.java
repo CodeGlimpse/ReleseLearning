6176feb2c42912cdb7777e293ccbfcce45841c43
mysonzw <1512605076@qq.com>
2022-11-02 11:35:48 +0800

    初始化Spring Initializer项目

---
+++ src/main/java/com/example/releselearning/ReleseLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.releselearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleseLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleseLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/releselearning/ReleseLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releselearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleseLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
