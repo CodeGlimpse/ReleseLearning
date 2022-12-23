5363a56404542b6a4b323bc4ab225703024e2e63
hsymr <1635696525@qq.com>
2022-12-23 17:09:08 +0800

    学生端提交文件，application里面添加了一点对文件的限制

--- src/main/java/com/example/releaselearning/controller/StuHomeworkController.java
+++ src/main/java/com/example/releaselearning/controller/StuHomeworkController.java
@@ -1,19 +1,25 @@
 package com.example.releaselearning.controller;
 
+import cn.hutool.http.server.HttpServerRequest;
+import com.example.releaselearning.entity.Homework;
 import com.example.releaselearning.entity.HwDetail;
 import com.example.releaselearning.entity.Student;
+import com.example.releaselearning.repository.HomeworkRepository;
 import com.example.releaselearning.repository.HwDetailRepository;
 import com.example.releaselearning.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.ObjectUtils;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping("/stu")
@@ -22,6 +28,8 @@ public class StuHomeworkController {
     private StudentRepository studentRepository;
     @Autowired
     private HwDetailRepository hwDetailRepository;
+    @Autowired
+    private HomeworkRepository homeworkRepository;
 
     @GetMapping("/homework/{studentId}")
     public String getHomeworkPage(Map<String, Object> map, @PathVariable
@@ -45,4 +53,82 @@ public class StuHomeworkController {
         }
         return "stuHomework";
     }
+
+    @GetMapping("/homework/findOne/{homeworkId}/{studentId}")
+    public String getOneHomework(Map<String,Object> map, @PathVariable
+    String homeworkId, @PathVariable String studentId){
+        return postOneHomework(map, homeworkId, studentId);
+    }
+
+    @PostMapping("/homework/findOne/{homeworkId}/{studentId}")
+    public String postOneHomework(Map<String,Object> map, @PathVariable
+    String homeworkId, @PathVariable String studentId) {
+        //单个作业提交
+        Optional<Homework> nowHomework = homeworkRepository.findById(homeworkId);
+        if(nowHomework.isPresent()){
+            Homework homework = nowHomework.get();
+            map.put("homeworkContent", homework);
+            List<HwDetail> hwDetails = hwDetailRepository.findHwDetailsByHomeworkId(homework);
+            for (HwDetail h : hwDetails){
+                if(h.getStudentId().getStudentId().equals(studentId)){
+                    map.put("homeworkDetail", h);
+                }
+            }
+        }
+
+        return "enter";
+    }
+
+    @PostMapping("/fileUpload")
+    public String fileUpload(@RequestParam("uploadFilee") MultipartFile multipartFiles, HttpServletRequest request){
+        //文件下载
+        //获取日期
+        String uploadDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        //设置文件保存路径，为运行目录下的uploadFile文件夹
+        String path = ClassUtils.getDefaultClassLoader().getResource("").getPath() +
+                "static/uploadFile/" + uploadDate;
+        File file = new File(path);
+        if(!file.isDirectory()){
+            file.mkdirs();
+        }
+        //文件重命名，避免重名
+        String oldName = multipartFiles.getOriginalFilename();
+        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."), oldName.length());
+        String fpath = "";
+        try{
+            File file1 = new File(file, newName);
+            multipartFiles.transferTo(file1);
+            //生成上传文件的访问路径
+            fpath = request.getScheme() + "://" + request.getServerName() + ":" +
+                    request.getServerPort() + "/uploadFile/" + uploadDate + "/" + newName;
+            System.out.println(fpath);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //将fpath存储进数据库
+        String homeworkId = request.getParameter("homeworkId");
+        String studentId = request.getParameter("studentId");
+        System.out.println(homeworkId);
+        System.out.println(studentId);
+        Optional<Homework> nowHomework = homeworkRepository.findById(homeworkId);
+        if(nowHomework.isPresent() && fpath.length() != 0){
+            Homework homework = nowHomework.get();
+            System.out.println(homework);
+            List<HwDetail> hwDetails = hwDetailRepository.findHwDetailsByHomeworkId(homework);
+            for (HwDetail h : hwDetails){
+                if(h.getStudentId().getStudentId().equals(studentId)){
+                    h.setHomeworkFile(fpath);
+                    h.setHomeworkStatus("已完成");
+                    hwDetailRepository.save(h);
+                    System.out.println(h);
+                }
+            }
+        }
+
+        return "last";
+
+
+    }
+
 }
